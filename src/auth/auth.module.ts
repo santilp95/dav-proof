1c952308872ce74556c578546b458ea691f4d1f2
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-04-02 15:10:28 -0500

    :bug: :zap: imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
 import { AuthController } from './auth.controller';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UsersService, JwtService, PrismaService],
 })
 export class AuthModule {}

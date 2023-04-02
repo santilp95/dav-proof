3bd29f5135b458f66f5203a5c232483f6c67d779
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-04-02 03:22:23 -0500

    :zap: entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  date_birth: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  mobile_pho: number;
+
+  @Column()
+  email: string;
+}
